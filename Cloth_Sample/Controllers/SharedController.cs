873146d56b9c6aa64fd4d10d366c02b4be1559f1
Ram <ramkchintale@gmail.com>
2020-07-24 02:36:22 +0530

    images

---
+++ Cloth_Sample/Controllers/SharedController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cloth_Sample.Controllers
+{
+    public class SharedController : Controller
+    {
+        // GET: Shared
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
